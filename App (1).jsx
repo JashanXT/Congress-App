9632fcab547ada568f727c93e7d827a51c55335d
JashanXT <124715965+JashanXT@users.noreply.github.com>
2023-04-19 10:24:34 -0400

    Add files via upload

---
+++ App (1).jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  return (
+    <div>
+      <h1>Chatroom</h1>
+      <ul>
+        {messages.map(message => (
+          <li>{message}</li>
+        ))}
+      </ul>
+      <input type="text" placeholder="Type your message here..." value={newMessage} onChange={e => setNewMessage(e.target.value)} />
+      <button type="button" onClick={() => setMessages([...messages, newMessage])}>Send Message</button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
